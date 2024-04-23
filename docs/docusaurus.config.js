569c62b19234e11dd98e3b29815233d322866074
CaioPaula <caio.paula@sou.inteli.edu.br>
2024-04-23 00:01:22 -0300

    refactor : arruma rota base do docusaurus

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -16,7 +16,7 @@ const config = {
   url: 'https://your-docusaurus-site.example.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/2024-T0008-EC05-G05/',
+  baseUrl: '/2024-1B-T08-EC06-G02/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
