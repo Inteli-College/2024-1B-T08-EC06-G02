74cfaf2a9bab58d31999b227a8e4bc0731c6c6fa
pedrocruz2 <pedro.cruz@sou.inteli.edu.br>
2024-05-28 17:22:45 -0300

    ahaha backend aee

---
+++ src/backend/database.py
@@ -0,0 +1,11 @@
+import sqlite3
+
+
+
+
+class DatabaseConnector():
+    def __init__(self, path):
+        self.path = path
+    
+
+    
---
+++ src/backend/server.py
@@ -0,0 +1,9 @@
+from fastapi import FastAPI, HTTPException
+from fastapi.responses import JSONResponses
+
+app = FastAPI(swagger_ui_parameters={"syntaxHighlight.theme": "obsidian"})
+
+
+@app.get("/users/{username}")
+async def read_user(username: str):
+    return {"message": f"Hello {username}"}
