eb01a018c0f324a9f4e8c3cc710340499c44b275
pedrocruz2 <pedro.cruz@sou.inteli.edu.br>
2024-06-03 16:49:42 -0300

    criacao estruturas backend

--- src/backend/database.py
+++ src/backend/database.py
@@ -1,11 +1,14 @@
 import sqlite3
 
 
-
-
 class DatabaseConnector():
     def __init__(self, path):
         self.path = path
+    
+    def insert_in_table(self,body)
+        conn = sqlite3.connect(self.path)
+        cursor = conn.cursor()
+        cursor.execute('INSERT INTO tabelax VALUES ?','body')
     
 
     
--- src/backend/server.py
+++ src/backend/server.py
@@ -7,3 +7,5 @@ app = FastAPI(swagger_ui_parameters={"syntaxHighlight.theme": "obsidian"})
 @app.get("/users/{username}")
 async def read_user(username: str):
     return {"message": f"Hello {username}"}
+
+
