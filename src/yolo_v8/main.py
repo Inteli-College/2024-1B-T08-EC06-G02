33c84304a484d2320cbd3e8bfcab7f0816fce798
Edustn <eduardo.santos@sou.inteli.edu.br>
2024-06-05 16:54:01 -0300

    feat: insere treinamento

--- src/yolo_v8/main.py
+++ src/yolo_v8/main.py
@@ -1,13 +1,15 @@
 from ultralytics import YOLO
 import cv2
 
-cap = cv2.VideoCapture(0)
+# cap = cv2.VideoCapture(0)
 model = YOLO("yolov8m.pt")
 
-if not cap.isOpened():
-    print("Error: Could not open video source.")
-else:
-    cap.release()
-    results = model.train(data="YOLODataset/dataset.yaml", epochs=2, imgsz=640)
-    results = model.predict(source=0, show=True)
+# if not cap.isOpened():
+#     print("Error: Could not open video source.")
+# else:
+#     cap.release()
+results = model.train(data="/home/grupo-02-t08/2024-1B-T08-EC06-G02/src/yolo_v8/YOLODataset/dataset.yaml", epochs=100, imgsz=640)
+val = model.val(results)
+print(val)
+# results = model.predict(source=0, show=True)
 
