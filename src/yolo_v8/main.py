93d49e04487eea800013024e9d5eaafd1583a924
lidiamariano <lidia.mariano@sou.inteli.edu.br>
2024-06-06 16:32:58 -0300

    feat: tune do modelo

--- src/yolo_v8/main.py
+++ src/yolo_v8/main.py
@@ -2,14 +2,23 @@ from ultralytics import YOLO
 import cv2
 
 cap = cv2.VideoCapture(0)
-model = YOLO("/home/lidia/2024-1B-T08-EC06-G02/runs/detect/train6/weights/best.pt")
 
 if not cap.isOpened():
     print("Error: Could not open video source.")
 else:
+    model = YOLO("/home/lidia/2024-1B-T08-EC06-G02/runs/detect/train6/weights/best.pt")
     cap.release()
-# results = model.train(data="/home/grupo-02-t08/2024-1B-T08-EC06-G02/src/yolo_v8/YOLODataset/dataset.yaml", epochs=100, imgsz=640)
-# val = model.val(results)
-# print(val)
-results = model.predict(source=0, show=True)
+
+    # Train the model
+    train_results = model.train(data="/home/grupo-02-t08/2024-1B-T08-EC06-G02/src/yolo_v8/YOLODataset/dataset.yaml", epochs=100, imgsz=640)
+    
+    # Validate the model
+    val_results = model.val()
+
+    # Tune hyperparameters
+    model.tune(data="/home/lidia/2024-1B-T08-EC06-G02/src/yolo_v8/YOLODataset/dataset.yaml", epochs=30, iterations=300, optimizer="AdamW", plots=False, save=False, val=False)
+    
+    # Predict using the tuned model
+    results = model.predict(source=0, show=True)
+
 
