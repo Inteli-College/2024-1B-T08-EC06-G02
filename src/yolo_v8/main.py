fac5fca68309c4b6c5491a4dc1305cf5d8f7c1e5
lidiamariano <lidia.mariano@sou.inteli.edu.br>
2024-06-07 14:16:44 -0300

    feat: tune do modelo

--- src/yolo_v8/main.py
+++ src/yolo_v8/main.py
@@ -9,16 +9,12 @@ else:
     model = YOLO("/home/lidia/2024-1B-T08-EC06-G02/runs/detect/train6/weights/best.pt")
     cap.release()
 
-    # Train the model
     train_results = model.train(data="/home/grupo-02-t08/2024-1B-T08-EC06-G02/src/yolo_v8/YOLODataset/dataset.yaml", epochs=100, imgsz=640)
     
-    # Validate the model
     val_results = model.val()
 
-    # Tune hyperparameters
     model.tune(data="/home/lidia/2024-1B-T08-EC06-G02/src/yolo_v8/YOLODataset/dataset.yaml", epochs=30, iterations=300, optimizer="AdamW", plots=False, save=False, val=False)
     
-    # Predict using the tuned model
     results = model.predict(source=0, show=True)
 
 
