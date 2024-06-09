f745c417164119e50d34c073cd7ed09075ee76fd
lidiamariano <lidia.mariano@sou.inteli.edu.br>
2024-06-09 16:55:41 -0300

    feat: ajustes no treinamento

--- src/yolo_v8/train.py
+++ src/yolo_v8/train.py
@@ -1,15 +1,8 @@
 from ultralytics import YOLO
-import cv2
 
-cap = cv2.VideoCapture(2)
-
-#carregamos o modelo já pré- treinado
+#carregamos o modelo já pré-treinado
 model = YOLO("yolov8m.pt")
 
-if not cap.isOpened():
-    print("Error: Could not open video source.")
-else:
-    cap.release()
-    # treinamento do modelo
-    results = model.train(data="YOLODataset/dataset.yaml", epochs=100, imgsz=640)
-    val = model.val(results)
+# treinamento do modelo
+results = model.train(data="YOLODataset/dataset.yaml", epochs=100, imgsz=640)
+val = model.val(results)
