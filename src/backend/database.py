4121d55e5eb726af98e788bf5474f2e079be6878
pedrocruz2 <pedro.cruz@sou.inteli.edu.com>
2024-06-09 16:12:38 -0300

    endpoints atualizados e funcionando

--- src/backend/database.py
+++ src/backend/database.py
@@ -13,54 +13,65 @@ class DatabaseConnector:
             print(e)
         return conn
 
-    def insert_in_table(self, username):
+    def insert_in_table(self, user):
         conn = self.create_connection()
         try:
             with conn:
-                sql = '''INSERT INTO users(username) VALUES(?)'''
+                sql = '''INSERT INTO Users(user_email, user_password, user_role, user_name) VALUES(?,?,?,?)'''
                 cur = conn.cursor()
-                cur.execute(sql, (username,))
+                cur.execute(sql, (user.user_email, user.user_password, user.user_role, user.user_name))
                 conn.commit()
                 return cur.lastrowid
         except Error as e:
             print(e)
             return None
 
-    def get_user(self, username):
+    def get_user(self, user_id):
         conn = self.create_connection()
         try:
             with conn:
-                sql = '''SELECT * FROM users WHERE username=?'''
+                sql = '''SELECT * FROM Users WHERE user_id=?'''
                 cur = conn.cursor()
-                cur.execute(sql, (username,))
-                row = cur.fetchone()
-                return row
+                cur.execute(sql, (user_id,))
+                return cur.fetchone()
         except Error as e:
             print(e)
             return None
 
-    def update_user(self, username):
+    def update_user(self, user_id, user):
         conn = self.create_connection()
         try:
             with conn:
-                sql = '''UPDATE users SET username=? WHERE username=?'''
+                sql = '''UPDATE Users SET user_email=?, user_password=?, user_role=?, user_name=? WHERE user_id=?'''
                 cur = conn.cursor()
-                cur.execute(sql, (username, username))
+                cur.execute(sql, (user.user_email, user.user_password, user.user_role, user.user_name, user_id))
                 conn.commit()
                 return cur.rowcount
         except Error as e:
             print(e)
             return None
 
-    def delete_user(self, username):
+    def delete_user(self, user_id):
         conn = self.create_connection()
         try:
             with conn:
-                sql = '''DELETE FROM users WHERE username=?'''
+                sql = '''DELETE FROM Users WHERE user_id=?'''
                 cur = conn.cursor()
-                cur.execute(sql, (username,))
+                cur.execute(sql, (user_id,))
                 conn.commit()
                 return cur.rowcount
+        except Error as e:
+            print(e)
+            return None
+
+    def get_all_users(self):
+        conn = self.create_connection()
+        try:
+            with conn:
+                sql = '''SELECT * FROM Users'''
+                cur = conn.cursor()
+                cur.execute(sql)
+                return cur.fetchall()  # Retorna todos os usuários
         except Error as e:
             print(e)
             return None
--- src/backend/server.py
+++ src/backend/server.py
@@ -1,35 +1,49 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Body
 from fastapi.responses import JSONResponse
+from pydantic import BaseModel
 from database import DatabaseConnector
 
 app = FastAPI(swagger_ui_parameters={"syntaxHighlight.theme": "obsidian"})
 
 connector = DatabaseConnector('repipe.db')
 
-@app.post("/users/{username}", status_code=201)
-async def create_user(username: str):
-    new_user = connector.insert_in_table(username)
-    if new_user is None:
-        raise HTTPException(status_code=404, detail="User not created")
-    return JSONResponse(content={"message": "User created"}, status_code=201)
+class User(BaseModel):
+    user_email: str
+    user_password: str
+    user_role: int
+    user_name: str
 
-@app.get("/users/{username}")
-async def read_user(username: str):
-    user = connector.get_user(username)
+@app.post("/createUser", status_code=201)
+async def create_user(user: User):
+    new_user_id = connector.insert_in_table(user)
+    if new_user_id is None:
+        raise HTTPException(status_code=404, detail="Usuário não foi criado")
+    return JSONResponse(content={"message": "Usuário Criado com Sucesso", "user_id": new_user_id}, status_code=201)
+
+@app.get("/getUser/{user_id}")
+async def get_user(user_id: int):
+    user = connector.get_user(user_id)
     if user is None:
-        raise HTTPException(status_code=404, detail="User not found")
-    return JSONResponse(content={"message": "User found"}, status_code=200)
+        raise HTTPException(status_code=404, detail="Usuário não encontrado")
+    return JSONResponse(content={"user": user}, status_code=200)
 
-@app.put("/users/{username}")
-async def update_user(username: str):
-    update_user = connector.update_user(username)
-    if update_user is None:
-        raise HTTPException(status_code=404, detail="User not found")
-    return JSONResponse(content={"message": "User updated"}, status_code=200)
+@app.put("/updateUser/{user_id}")
+async def update_user(user_id: int, user: User):
+    update_count = connector.update_user(user_id, user)
+    if update_count == 0:
+        raise HTTPException(status_code=404, detail="Usuário não foi atualizado")
+    return JSONResponse(content={"message": "Usuário atualizado com sucesso"}, status_code=200)
 
-@app.delete("/users/{username}")
-async def delete_user(username: str):
-    delete_user = connector.delete_user(username)
-    if not delete_user:
-        raise HTTPException(status_code=404, detail="User not deleted")
-    return JSONResponse(content={"message": "User deleted"}, status_code=200)   
+@app.delete("/deleteUser/{user_id}")
+async def delete_user(user_id: int):
+    delete_count = connector.delete_user(user_id)
+    if delete_count == 0:
+        raise HTTPException(status_code=404, detail="Usuário não foi deletado")
+    return JSONResponse(content={"message": "Usuário deletado com sucesso"}, status_code=200)
+
+@app.get("/getUsers")
+async def get_users(): 
+    users = connector.get_all_users()
+    if not users:
+        raise HTTPException(status_code=404, detail="Nenhum usuário encontrado")
+    return JSONResponse(content={"users": users}, status_code=200)
