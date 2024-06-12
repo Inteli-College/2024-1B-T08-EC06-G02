b5f7e1cf8661a0628e8c7c5defb1d9679a5ea549
lidiamariano <lidia.mariano@sou.inteli.edu.br>
2024-06-12 17:31:15 -0300

    refactor : ajuste codigo modelo

--- src/yolo_v8/predict.py
+++ src/yolo_v8/predict.py
@@ -1,13 +1,14 @@
 from ultralytics import YOLO
 import cv2
 
-cap = cv2.VideoCapture(2)
-
-#carregamos o modelo já pré- treinado
+# Carregamos o modelo já pré-treinado
 model = YOLO("runs/detect/train6/weights/best.pt")
 
-if not cap.isOpened():
-    print("Error: Could not open video source.")
+image_path = ""
+
+image = cv2.imread(image_path)
+
+if image is None:
+    print("Error: Could not load image.")
 else:
-    cap.release()
-    results = model.predict(source=2, show=True)
+    results = model.predict(source=image, show=True)
